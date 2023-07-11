f2f9bc232953a616d7b9a33b9c32ed7fa5efd58f
David Lemaignent <david.lemaignent@univ-rouen.fr>
2023-07-11 16:35:05 +0200

    Update launch-esup-dss-client.sh

--- src/izpack/launch-esup-dss-client.sh
+++ src/izpack/launch-esup-dss-client.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-%{INSTALL_PATH}/java/linux/zulu@jdk.zulu.version@-linux_x64/bin/java --add-exports javafx.graphics/com.sun.javafx.application=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED -jar %{INSTALL_PATH}/esup-dss-client.jar &
+%{INSTALL_PATH}/java/linux/zulu%{jdk.zulu.version}-linux_x64/bin/java --add-exports javafx.graphics/com.sun.javafx.application=ALL-UNNAMED --add-exports javafx.graphics/com.sun.javafx.tk=ALL-UNNAMED -jar %{INSTALL_PATH}/esup-dss-client.jar &
