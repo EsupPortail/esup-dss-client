7a49903d10ecf6b8a71229d2b24293eadb9b967c
David Lemaignent <david.lemaignent@univ-rouen.fr>
2024-03-29 13:53:51 +0100

    disable code signing

--- src/izpack/pkg.sh
+++ src/izpack/pkg.sh
@@ -5,8 +5,10 @@ sudo codesign --sign "Developer ID Application: ****" --entitlements $macbundle.
 sudo codesign --sign "Developer ID Application: ****" $macbundle.path/esup-dss-client.app
 sudo chmod +x $macbundle.path/esup-dss-client.app/Contents/Scripts/*
 cd $macbundle.path/esup-dss-client.app
-pkgbuild --version 1.2.0.0 --identifier org.esupportail.esup-dss-client --install-location /Applications/esup-dss-client.app --scripts Contents/Scripts --identifier org.esupportail.esup-dss-client --sign "Developer ID Installer: ****" --root . Distribution.pkg
+#pkgbuild --version 1.2.0.0 --identifier org.esupportail.esup-dss-client --install-location /Applications/esup-dss-client.app --scripts Contents/Scripts --identifier org.esupportail.esup-dss-client --sign "Developer ID Installer: ****" --root . Distribution.pkg
+pkgbuild --version 1.2.0.0 --identifier org.esupportail.esup-dss-client --install-location /Applications/esup-dss-client.app --scripts Contents/Scripts --identifier org.esupportail.esup-dss-client --root . Distribution.pkg
 productbuild --synthesize --package Distribution.pkg Distribution.xml
 #rm Distribution.pkg
 cd ../
-productbuild --distribution esup-dss-client.app/Distribution.xml --sign "Developer ID Installer: ****" --resources esup-dss-client.app/Contents/Resources --package-path esup-dss-client.app esup-dss-client.pkg
+#productbuild --distribution esup-dss-client.app/Distribution.xml --sign "Developer ID Installer: ****" --resources esup-dss-client.app/Contents/Resources --package-path esup-dss-client.app esup-dss-client.pkg
+productbuild --distribution esup-dss-client.app/Distribution.xml --resources esup-dss-client.app/Contents/Resources --package-path esup-dss-client.app esup-dss-client.pkg
