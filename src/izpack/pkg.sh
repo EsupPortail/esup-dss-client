1b089c2370a73f6831045c0e940565b6044dbf38
David Lemaignent <david.lemaignent@univ-rouen.fr>
2024-04-05 16:15:14 +0200

    1.2.1

--- src/izpack/pkg.sh
+++ src/izpack/pkg.sh
@@ -5,8 +5,8 @@ sudo codesign --sign "Developer ID Application: ****" --entitlements $macbundle.
 sudo codesign --sign "Developer ID Application: ****" $macbundle.path/esup-dss-client.app
 sudo chmod +x $macbundle.path/esup-dss-client.app/Contents/Scripts/*
 cd $macbundle.path/esup-dss-client.app
-#pkgbuild --version 1.2.0.0 --identifier org.esupportail.esup-dss-client --install-location /Applications/esup-dss-client.app --scripts Contents/Scripts --identifier org.esupportail.esup-dss-client --sign "Developer ID Installer: ****" --root . Distribution.pkg
-pkgbuild --version 1.2.0.0 --identifier org.esupportail.esup-dss-client --install-location /Applications/esup-dss-client.app --scripts Contents/Scripts --identifier org.esupportail.esup-dss-client --root . Distribution.pkg
+#pkgbuild --version 1.2.1.0 --identifier org.esupportail.esup-dss-client --install-location /Applications/esup-dss-client.app --scripts Contents/Scripts --identifier org.esupportail.esup-dss-client --sign "Developer ID Installer: ****" --root . Distribution.pkg
+pkgbuild --version 1.2.1.0 --identifier org.esupportail.esup-dss-client --install-location /Applications/esup-dss-client.app --scripts Contents/Scripts --identifier org.esupportail.esup-dss-client --root . Distribution.pkg
 productbuild --synthesize --package Distribution.pkg Distribution.xml
 #rm Distribution.pkg
 cd ../
